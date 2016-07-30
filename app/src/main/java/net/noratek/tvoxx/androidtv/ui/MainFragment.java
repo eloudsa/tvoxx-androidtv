b8cbc16be84b7eb44934733be025736b91ea1a03
Said Eloudrhiri <info@noratek.net>
2016-07-31 01:49:22 +0200

    First presenter

--- app/src/main/java/net/noratek/tvoxx/androidtv/ui/MainFragment.java
+++ app/src/main/java/net/noratek/tvoxx/androidtv/ui/MainFragment.java
@@ -1,8 +1,17 @@
 package net.noratek.tvoxx.androidtv.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v17.leanback.app.BrowseFragment;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.HeaderItem;
+import android.support.v17.leanback.widget.ListRow;
+import android.support.v17.leanback.widget.ListRowPresenter;
+import android.support.v17.leanback.widget.Presenter;
 import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import net.noratek.tvoxx.androidtv.R;
 
@@ -10,11 +19,15 @@ import net.noratek.tvoxx.androidtv.R;
 public class MainFragment extends BrowseFragment {
     private static final String TAG = MainFragment.class.getSimpleName();
 
+    private ArrayObjectAdapter mRowsAdapter;
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
         setupUIElements();
+
+        loadRows();
     }
 
 
@@ -32,5 +45,46 @@ public class MainFragment extends BrowseFragment {
         // set search icon color
         setSearchAffordanceColor(ContextCompat.getColor(getActivity(), R.color.search_opaque));
 
+    }
+
+    private void loadRows() {
+        mRowsAdapter = new ArrayObjectAdapter(new ListRowPresenter());
+
+        /* GridItemPresenter */
+        HeaderItem gridItemPresenterHeader = new HeaderItem(0, "GridItemPresenter");
+
+        GridItemPresenter mGridPresenter = new GridItemPresenter();
+        ArrayObjectAdapter gridRowAdapter = new ArrayObjectAdapter(mGridPresenter);
+        gridRowAdapter.add("ITEM 1");
+        gridRowAdapter.add("ITEM 2");
+        gridRowAdapter.add("ITEM 3");
+        mRowsAdapter.add(new ListRow(gridItemPresenterHeader, gridRowAdapter));
+
+        /* set */
+        setAdapter(mRowsAdapter);
+    }
+
+    private class GridItemPresenter extends Presenter {
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent) {
+            TextView view = new TextView(parent.getContext());
+            view.setLayoutParams(new ViewGroup.LayoutParams(getResources().getInteger(R.integer.grid_item_width), getResources().getInteger(R.integer.grid_item_height)));
+            view.setFocusable(true);
+            view.setFocusableInTouchMode(true);
+            view.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.default_background));
+            view.setTextColor(Color.WHITE);
+            view.setGravity(Gravity.CENTER);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, Object item) {
+            ((TextView) viewHolder.view).setText((String) item);
+        }
+
+        @Override
+        public void onUnbindViewHolder(ViewHolder viewHolder) {
+
+        }
     }
 }
