33726e70279311889f4a8387e9566cf959a50dcd
Said Eloudrhiri <info@noratek.net>
2016-08-15 01:34:58 +0200

    refactoring

--- app/src/main/java/net/noratek/tvoxx/androidtv/ui/MainFragment.java
+++ app/src/main/java/net/noratek/tvoxx/androidtv/ui/MainFragment.java
@@ -1,8 +1,6 @@
 package net.noratek.tvoxx.androidtv.ui;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v17.leanback.app.BrowseFragment;
 import android.support.v17.leanback.widget.ArrayObjectAdapter;
@@ -10,30 +8,18 @@ import android.support.v17.leanback.widget.HeaderItem;
 import android.support.v17.leanback.widget.ListRow;
 import android.support.v17.leanback.widget.ListRowPresenter;
 import android.support.v17.leanback.widget.OnItemViewClickedListener;
-import android.support.v17.leanback.widget.OnItemViewSelectedListener;
 import android.support.v17.leanback.widget.Presenter;
 import android.support.v17.leanback.widget.Row;
 import android.support.v17.leanback.widget.RowPresenter;
 import android.support.v4.content.ContextCompat;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import net.noratek.tvoxx.androidtv.R;
-import net.noratek.tvoxx.androidtv.data.cache.SpeakersCache;
 import net.noratek.tvoxx.androidtv.data.cache.TalksCache;
-import net.noratek.tvoxx.androidtv.data.manager.SpeakerManager;
 import net.noratek.tvoxx.androidtv.data.manager.TalkManager;
-import net.noratek.tvoxx.androidtv.event.SpeakersEvent;
 import net.noratek.tvoxx.androidtv.event.TalksEvent;
-import net.noratek.tvoxx.androidtv.model.CardModel;
-import net.noratek.tvoxx.androidtv.model.SpeakerModel;
 import net.noratek.tvoxx.androidtv.model.TalkFullModel;
-import net.noratek.tvoxx.androidtv.ui.presenter.CardPresenter;
-import net.noratek.tvoxx.androidtv.ui.presenter.SpeakerPresenter;
 import net.noratek.tvoxx.androidtv.ui.presenter.TalkPresenter;
 import net.noratek.tvoxx.androidtv.utils.Constants;
-import net.noratek.tvoxx.androidtv.utils.Utils;
 
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EFragment;
@@ -50,26 +36,11 @@ public class MainFragment extends BrowseFragment {
     private static final String TAG = MainFragment.class.getSimpleName();
 
     @Bean
-    SpeakersCache speakersCache;
-
-    @Bean
-    SpeakerManager speakerManager;
-
-    @Bean
     TalksCache talksCache;
 
     @Bean
     TalkManager talkManager;
 
-
-
-    private ArrayObjectAdapter mRowsAdapter;
-
-    // Headers
-    HeaderItem mSpeakerHeaderPresenter;
-
-    // Background image
-   // private BackgroundImageManager mBackgroundImageManager;
 
 
     @Override
@@ -78,11 +49,7 @@ public class MainFragment extends BrowseFragment {
 
         EventBus.getDefault().register(this);
 
-        // Prepare the manager that maintains the same background image between activities.
-        //mBackgroundImageManager = new BackgroundImageManager(getActivity());
-
         setupUIElements();
-
         setupEventListeners();
 
         try {
@@ -98,7 +65,6 @@ public class MainFragment extends BrowseFragment {
     @Override
     public void onStop() {
         EventBus.getDefault().unregister(this);
-        //mBackgroundImageManager.cancel();
         super.onStop();
     }
 
@@ -115,127 +81,11 @@ public class MainFragment extends BrowseFragment {
 
         // set search icon color
         setSearchAffordanceColor(ContextCompat.getColor(getActivity(), R.color.search_opaque));
-
-        /*
-        // change the background image
-        BackgroundManager backgroundManager = BackgroundManager.getInstance(getActivity());
-        backgroundManager.attach(getActivity().getWindow());
-        backgroundManager.setDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.default_background));
-        */
     }
 
 
     private void setupEventListeners() {
         setOnItemViewClickedListener(new ItemViewClickedListener());
-        //setOnItemViewSelectedListener(new ItemViewSelectedListener());
-    }
-
-
-    private final class ItemViewSelectedListener implements OnItemViewSelectedListener {
-        @Override
-        public void onItemSelected(Presenter.ViewHolder itemViewHolder, Object item,
-                                   RowPresenter.ViewHolder rowViewHolder, Row row) {
-
-            String imageUrl = null;
-
-            if (item instanceof CardModel) {
-                imageUrl = ((CardModel) item).getCardImageUrl();
-
-            } else if (item instanceof SpeakerModel) {
-                imageUrl = ((SpeakerModel) item).getAvatarUrl();
-
-            }  else if (item instanceof TalkFullModel) {
-                imageUrl = ((TalkFullModel) item).getThumbnailUrl();
-            }
-
-            Uri backgroundURI;
-
-            if (imageUrl != null) {
-                backgroundURI = Uri.parse(imageUrl);
-            } else {
-                backgroundURI = Utils.getUri(getContext(), R.drawable.default_background);
-            }
-
-            //mBackgroundImageManager.updateBackgroundWithDelay(backgroundURI);
-        }
-    }
-
-
-
-    private void loadRows() {
-        mRowsAdapter = new ArrayObjectAdapter(new ListRowPresenter());
-
-        /* GridItemPresenter */
-        HeaderItem gridItemPresenterHeader = new HeaderItem(0, "GridItemPresenter");
-
-        GridItemPresenter mGridPresenter = new GridItemPresenter();
-        ArrayObjectAdapter gridRowAdapter = new ArrayObjectAdapter(mGridPresenter);
-        gridRowAdapter.add("ITEM 1");
-        gridRowAdapter.add("ITEM 2");
-        gridRowAdapter.add("ITEM 3");
-        mRowsAdapter.add(new ListRow(gridItemPresenterHeader, gridRowAdapter));
-
-        /* CardPresenter */
-        HeaderItem cardPresenterHeader = new HeaderItem(1, "CardPresenter");
-        CardPresenter cardPresenter = new CardPresenter();
-        ArrayObjectAdapter cardRowAdapter = new ArrayObjectAdapter(cardPresenter);
-
-        for(int i=0; i<10; i++) {
-            CardModel cardModel = new CardModel();
-            cardModel.setCardImageUrl("http://heimkehrend.raindrop.jp/kl-hacker/wp-content/uploads/2014/08/DSC02580.jpg");
-            cardModel.setTitle("title" + i);
-            cardModel.setContent("studio" + i);
-            cardRowAdapter.add(cardModel);
-        }
-        mRowsAdapter.add(new ListRow(cardPresenterHeader, cardRowAdapter));
-
-        /* Speakers */
-        mSpeakerHeaderPresenter = new HeaderItem(Constants.HEADER_SPEAKER, getString(R.string.speakers));
-        SpeakerPresenter speakerPresenter = new SpeakerPresenter();
-        ArrayObjectAdapter speakersRowAdapter = new ArrayObjectAdapter(speakerPresenter);
-        mRowsAdapter.add(new ListRow(mSpeakerHeaderPresenter, speakersRowAdapter));
-
-        try {
-            speakerManager.fetchAllSpeakers();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        try {
-            talkManager.fetchAllTalks();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        setAdapter(mRowsAdapter);
-    }
-
-
-    private class GridItemPresenter extends Presenter {
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent) {
-
-            TextView view = new TextView(parent.getContext());
-            view.setLayoutParams(new ViewGroup.LayoutParams(getResources().getDimensionPixelSize(R.dimen.grid_item_width), getResources().getDimensionPixelSize(R.dimen.grid_item_height)));
-            view.setFocusable(true);
-            view.setFocusableInTouchMode(true);
-            view.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.default_background));
-            view.setTextColor(Color.WHITE);
-            view.setGravity(Gravity.CENTER);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder viewHolder, Object item) {
-            ((TextView) viewHolder.view).setText((String) item);
-        }
-
-        @Override
-        public void onUnbindViewHolder(ViewHolder viewHolder) {
-
-        }
     }
 
 
@@ -255,28 +105,9 @@ public class MainFragment extends BrowseFragment {
     }
 
 
-
+    //
     // Events
-
-    @Subscribe
-    public void onMessageEvent(SpeakersEvent speakersEvent) {
-
-        List<SpeakerModel> speakersModel = speakersCache.getData();
-        if (speakersModel == null) {
-            return;
-        }
-
-        SpeakerPresenter speakerPresenter = new SpeakerPresenter();
-        ArrayObjectAdapter speakersRowAdapter = new ArrayObjectAdapter(speakerPresenter);
-
-        // display speakers
-        for (SpeakerModel speaker : speakersModel) {
-            speakersRowAdapter.add(speaker);
-        }
-
-        mRowsAdapter.replace(Constants.HEADER_SPEAKER, new ListRow(mSpeakerHeaderPresenter, speakersRowAdapter));
-    }
-
+    //
 
     @Subscribe
     public void onMessageEvent(TalksEvent talksEvent) {
@@ -297,10 +128,9 @@ public class MainFragment extends BrowseFragment {
 
         for(Map.Entry<String,List<TalkFullModel>> track : tracks.entrySet()) {
 
-            //String trackTitle = track.getKey();
             List<TalkFullModel> talks = track.getValue();
-
             if ((talks != null) && (talks.size() > 0)) {
+                // get the track title with its proper case
                 String trackTitle = talks.get(0).getTrackTitle();
 
                 trackHeaderPresenter = new HeaderItem(headerId++, trackTitle);
@@ -317,18 +147,6 @@ public class MainFragment extends BrowseFragment {
 
 
         setAdapter(rowsAdapter);
-
-        /*
-        SpeakerPresenter speakerPresenter = new SpeakerPresenter();
-        ArrayObjectAdapter speakersRowAdapter = new ArrayObjectAdapter(speakerPresenter);
-
-        // display speakers
-        for (SpeakerModel speaker : speakersModel) {
-            speakersRowAdapter.add(speaker);
-        }
-
-        mRowsAdapter.replace(Constants.HEADER_SPEAKER, new ListRow(mSpeakerHeaderPresenter, speakersRowAdapter));
-        */
     }
 
 
